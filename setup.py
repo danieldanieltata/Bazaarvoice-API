988443b4d4811f015957268b833ef090447dd5ba
Daniel Talor <Daniel@revuze.it>
2016-07-28 18:39:17 +0300

    Added requirements
    Signed-off-by: Daniel Talor <Daniel@revuze.it>

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Bazaarvoice-API',
+      description='Bazaarvoice-API module',
+      author='Daniel Talor Moses',
+      author_email='danieldanieltalortalor@gmail.com',
+      url='https://github.com/danieldanieltata/Bazaarvoice-API',
+      packages=['bazaarvoice_api'], requires=['requests']
+      )
