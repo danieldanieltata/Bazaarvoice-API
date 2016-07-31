87ee3a0a5143c9092c24af5d6d7c8026b5a0d6bf
Daniel Talor <Daniel@revuze.it>
2016-07-31 10:54:00 +0300

    Update: more nice looking way to get products, now return reviews as default
    Signed-off-by: Daniel Talor <Daniel@revuze.it>

--- bazaarvoice_api/__init__.py
+++ bazaarvoice_api/__init__.py
@@ -1,4 +1,6 @@
 import json
+from math import floor
+
 import requests
 
 from bazaarvoice_api.product import Product
@@ -14,7 +16,6 @@ class BazaarvoiceAPI:
     searched_brand = None
 
     reviews_list = []
-    start_url = None
 
     def __init__(self, api_key, searched_brand):
         self.api_key = api_key
@@ -26,21 +27,6 @@ class BazaarvoiceAPI:
         if 'http://' in searched_brand:
             raise Exception('You cant provide url')
 
-    def start_extracting_products(self):
-        self.start_url = self.make_url()
-
-        page_content = requests.get(self.start_url, headers=self.headers).text
-
-        try:
-            json_product_data = json.loads(page_content)
-        except:
-            raise Exception("Page is not json or valid json")
-
-        self._check_response(json_product_data)
-        page_data_handler = self._get_products(json_product_data)
-
-        return page_data_handler
-
     def make_url(self):
         searched_brand = self.searched_brand
         api_key = self.api_key
@@ -48,7 +34,7 @@ class BazaarvoiceAPI:
         base_url = 'https://api.bazaarvoice.com/data/'
 
         start_url = base_url + "products.json?apiversion=5.4&passkey=" + api_key + \
-                    "&ExcludeFamily=true&limit=100&include=Categories&search=" + searched_brand
+                    "&ExcludeFamily=true&limit=100&include=Categories&search=" + searched_brand + '&offset=0'
 
         return start_url
 
@@ -65,29 +51,56 @@ class BazaarvoiceAPI:
 
             raise Exception('Bad response: ' + errors_str_handler)
 
-    def _get_products(self, json_product_data):
-        products = json_product_data.get('Results', None)
+    def get_product(self):
+        products_url = self.make_url()
 
-        if products:
-            for product_item in products:
-                product_obj = Product(product_item, self.start_url)
+        products = self._get_products(products_url)
+        for products_list in products:
+            for product_data in products_list:
+                product_obj = Product(product_data, products_url)
+
+                review_list = []
+                for review_obj in product_obj.get_review():
+                        review_list.append(review_obj)
+
+                product_obj.__setattr__('reviews', review_list)
 
                 yield product_obj
 
+    def _get_products(self, products_url):
+        products_content = requests.get(products_url, headers=self.headers).text
+        products_json = json.loads(products_content)
+
+        self._check_response(products_json)
+
+        product_max_offset = int(floor(int(products_json['TotalResults']) / 100.0)) * 100
+        for i in range(0, product_max_offset + 100, 100):
+            offset = i
+
+            products_url = self._make_next_page_url(offset, products_url)
+
+            yield products_json['Results']
+
+            products_content = requests.get(products_url, headers=self.headers).text
+            products_json = json.loads(products_content)
+
+    @staticmethod
+    def _make_next_page_url(offset, product_url):
+        new_offset = offset + 100
+
+        current_offset_str = 'offset=%d' % offset
+        new_offset_str = 'offset=%d' % new_offset
+
+        new_reviews_url = product_url.replace(current_offset_str, new_offset_str)
+
+        return new_reviews_url
+
 
 bazzare = BazaarvoiceAPI('caKM8b618jM7usc0wJfL98n9Ospp9LvgNilEMqrBHOXSo', 'Aussie')
-gen = bazzare.start_extracting_products()
 
 data_container = []
 
-for prod in gen:
-    reviews_list = []
-    review_gen = prod.get_review()
-
-    count = 0
-    for review in review_gen:
-        reviews_list.append(review)
-
-    prod.reviews = reviews_list
+for prod in bazzare.get_product():
     data_container.append(prod)
+
 pass
